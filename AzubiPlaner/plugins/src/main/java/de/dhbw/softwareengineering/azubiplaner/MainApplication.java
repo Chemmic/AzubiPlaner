3116dfcc97bee0a90a19a81b7327aadf632f7f00
Matteo Staar <matteo.staar@cjt.de>
2024-02-29 13:07:13 +0100

    fixed wrong path in entityScan/JpaRepo location

--- AzubiPlaner/plugins/src/main/java/de/dhbw/softwareengineering/azubiplaner/MainApplication.java
+++ AzubiPlaner/plugins/src/main/java/de/dhbw/softwareengineering/azubiplaner/MainApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "de.dhbw.softwareengineering.deinProjektname.domain")
-@EntityScan("de.dhbw.softwareengineering.deinProjektname.domain.entities")
+@EnableJpaRepositories(basePackages = "de.dhbw.softwareengineering.azubiplaner.domain")
+@EntityScan("de.dhbw.softwareengineering.azubiplaner.domain.entities")
 public class MainApplication {
 
     public static void main(String[] args) {
